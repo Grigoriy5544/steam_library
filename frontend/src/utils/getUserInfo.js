4c6a789051d6bd65954e8f1d967aefe60ecc5f7f
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-08-05 23:48:35 +0400

    fix bag

--- frontend/src/utils/getUserInfo.js
+++ frontend/src/utils/getUserInfo.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function getUserInfo(steam_id) {
 	const { data } = await axios.get(
-		`${process.env.REACT_APP_API_URL}/getUserInfo?steam_id=${steam_id}`
+		process.env.REACT_APP_API_URL + `getUserInfo?steam_id=${steam_id}`
 	)
 	return data
 }
