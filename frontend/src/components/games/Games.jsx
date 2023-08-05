a88ca049c80e99a5f07368c062b1e95bd1a3e9b9
Grigoriy5544 <gregkotsenko@yandex.ru>
2023-08-06 00:00:24 +0400

    fix bag

--- frontend/src/components/games/Games.jsx
+++ frontend/src/components/games/Games.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './style.module.css'
 
+const runGame = appid => {
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
+    .test(navigator.userAgent)) {
+      window.open(`steam://rungame/${appid}/76561202255233023/`, '_blank')
+    }
+}
+
 export const Games = ({games}) => {
   return ((
       <div className={styles.games}>
       {games ? games.map((game, key) => (
         <div key={game.appid} className={styles.card} 
-        onClick={() => window.open(`steam://rungame/${game.appid}/76561202255233023/`, '_blank')}>
+        onClick={() => runGame(game.appid)}>
           <img src={`https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`} alt={game.name} className={styles.image}/>
           <p key={game.appid} className={styles.name}>{game.name}</p>
           {/* <p className={styles.text}> Сыграно {Math.round(game.playtime_forever / 60)} ч.</p> */}
